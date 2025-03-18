926177a1696f4792ac8ffa1df9e1036ca36c1e30
Cesar Alas <csaralas16@gmail.com>
2025-03-18 00:53:32 -0600

    fix url message confirmation

--- helpers/emails.js
+++ helpers/emails.js
@@ -21,14 +21,14 @@ const emailRegistro = async (datos) => {
         subject: 'Confirmación de cuenta',
         text: `Hola ${nombre}, confirma tu cuenta en Bienes Raíces.
     
-        Confirma tu cuenta en el siguiente enlace: ${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/auth/confirmar/${token}
+        Confirma tu cuenta en el siguiente enlace: ${process.env.BACKEND_URL}/auth/confirmar/${token}
     
         Si tú no creaste una cuenta en Bienes Raíces, ignora este email.`,
         html: `
             <p>Hola ${nombre}, confirma tu cuenta en <strong>Bienes Raíces</strong></p>
             
             <p>Confirma tu cuenta en el siguiente enlace: <br> 
-            <a href="${process.env.BACKEND_URL}${process.env.PORT ? `:${process.env.PORT}` : ''}/auth/confirmar/${token}" target="_blank">
+            <a href="${process.env.BACKEND_URL}/auth/confirmar/${token}" target="_blank">
             Confirmar cuenta</a></p>
     
             <p>Si tú no creaste una cuenta en Bienes Raíces, ignora este email.</p>
@@ -59,7 +59,7 @@ const emailRecuperarPassword = async (datos) => {
       text: `Hola ${nombre}, has solicitado restablecer tu contraseña.
   
       Para restablecer tu contraseña, ingresa al siguiente enlace: 
-      ${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/auth/olvide-password/${token}
+      ${process.env.BACKEND_URL}/auth/olvide-password/${token}
   
       Si no solicitaste restablecer tu contraseña, ignora este mensaje.`,
       html: `
@@ -67,7 +67,7 @@ const emailRecuperarPassword = async (datos) => {
         
         <p>Para restablecer tu contraseña, haz clic en el siguiente enlace:</p>
         <p>
-          <a href="${process.env.BACKEND_URL}${process.env.PORT ? `:${process.env.PORT}` : ''}/auth/olvide-password/${token}" target="_blank" 
+          <a href="${process.env.BACKEND_URL}/auth/olvide-password/${token}" 
              style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: #ffffff; text-decoration: none; border-radius: 5px;">
              Restablecer Contraseña
           </a>
