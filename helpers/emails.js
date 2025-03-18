b677e7116e8362ba722fcddb20776dd1917f8b90
Cesar Alas <csaralas16@gmail.com>
2025-03-18 00:41:30 -0600

    fix html message

--- helpers/emails.js
+++ helpers/emails.js
@@ -28,7 +28,7 @@ const emailRegistro = async (datos) => {
             <p>Hola ${nombre}, confirma tu cuenta en <strong>Bienes Raíces</strong></p>
             
             <p>Confirma tu cuenta en el siguiente enlace: <br> 
-            <a href="${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/auth/confirmar/${token}" target="_blank">
+            <a href="${process.env.BACKEND_URL}${process.env.PORT ? `:${process.env.PORT}` : ''}/auth/confirmar/${token}" target="_blank">
             Confirmar cuenta</a></p>
     
             <p>Si tú no creaste una cuenta en Bienes Raíces, ignora este email.</p>
@@ -67,7 +67,7 @@ const emailRecuperarPassword = async (datos) => {
         
         <p>Para restablecer tu contraseña, haz clic en el siguiente enlace:</p>
         <p>
-          <a href="${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/auth/olvide-password/${token}" 
+          <a href="${process.env.BACKEND_URL}${process.env.PORT ? `:${process.env.PORT}` : ''}/auth/olvide-password/${token}" target="_blank" 
              style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: #ffffff; text-decoration: none; border-radius: 5px;">
              Restablecer Contraseña
           </a>
